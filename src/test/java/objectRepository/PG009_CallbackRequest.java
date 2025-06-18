ce17cada5a32881fdacbcb9ecebce5a6d46c2331
AsifRahman48 <mdasifrahmanm01@gmail.com>
2025-06-18 11:35:43 +0600

    Call back Request added

--- src/test/java/objectRepository/PG009_CallbackRequest.java
+++ src/test/java/objectRepository/PG009_CallbackRequest.java
@@ -8,7 +8,7 @@ public class PG009_CallbackRequest {
     public static By navBarCallbackRequest = By.xpath("/html/body/app-root/layout/classy-layout/fuse-vertical-navigation/div/div[2]/fuse-vertical-navigation-collapsable-item[3]/div[2]/fuse-vertical-navigation-collapsable-item[1]/div[2]/fuse-vertical-navigation-basic-item[3]/div/a/div/div/span");
 
     public static By startDate = By.xpath("/html/body/app-root/layout/classy-layout/div/div[2]/app-cheque-stop-request/div/div/form/div[1]/div/mat-form-field[1]/div[1]/div[2]/div[2]");
-    public static By SelectStartDate = By.xpath("//span[normalize-space()='29']");
+    public static By SelectStartDate = By.xpath("//span[normalize-space()='28']");
 
     public static By startTime = By.xpath("/html/body/app-root/layout/classy-layout/div/div[2]/app-cheque-stop-request/div/div/form/div[1]/div/mat-form-field[2]/div[1]/div[2]/div[2]");
     public static By SelectStartTime = By.xpath("//span[normalize-space()='3']");
