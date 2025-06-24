3649d55f1206f50e59e5b1bd0057bd233d442d8a
AsifRahman48 <mdasifrahmanm01@gmail.com>
2025-06-24 18:25:29 +0600

    Own Card to Account added

--- src/test/java/objectRepository/PG014_CardToAccountOwn.java
+++ src/test/java/objectRepository/PG014_CardToAccountOwn.java
@@ -7,13 +7,16 @@ public class PG014_CardToAccountOwn {
     public static By navBarCardToAccount = By.xpath("//span[contains(text(),'Card to Account')]");
     public static By selectOwnAccount = By.xpath("/html/body/app-root/layout/classy-layout/div/div[2]/app-card-to-bank/div/div/form/div[2]/div[2]/div/button");
 
-    public static By amountSlab = By.xpath("/html/body/app-root/layout/classy-layout/div/div[2]/app-card-to-bank-form/div/div/form/div/div[4]/div/div/button[1]");
-    public static By nextButton = By.xpath("/html/body/app-root/layout/classy-layout/div/div[2]/app-card-to-bank-form/div/div/form/div/div[8]/div/button[2]");
+    public static By amount = By.xpath("//input[@id='mat-input-0']");
+    public static By availableBalance = By.xpath("//div[@class='flex justify-between pb-4 ng-star-inserted']");
+    public static By nextButton = By.xpath("//button[normalize-space()='Next']");
 
     public static By CheckBox = By.xpath("//div[@class='mdc-checkbox']");
     public static By SendOTP = By.xpath("//button[normalize-space()='Next']");
 
-    public static By EnterOTP = By.xpath("//*[@id=\"mat-mdc-dialog-0\"]/div/div/app-otp-dialog/div[1]/div[1]/code-input/span[1]/input");
-    public static By OTPNextButton = By.xpath("//button[normalize-space()='Next']");
+    public static By EnterOTP = By.xpath("//span[1]//input[1]");
+    public static By OTPNextButton = By.xpath("//div[@class='flex space-x-2 justify-center']//button[@class='btn-primary'][normalize-space()='Next']");
+
+    public static By transactionStatus = By.xpath("//span[@class='text-md font-normal']");
     public static By GoToHomeButton = By.xpath("//button[normalize-space()='Go to Home']");
 }
--- src/test/java/stepdefinitions/PM015_CardToAccountOwn.java
+++ src/test/java/stepdefinitions/PM015_CardToAccountOwn.java
@@ -1,9 +1,16 @@
 package stepdefinitions;
 
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Then;
+import objectRepository.PG012_Donation;
 import objectRepository.PG014_CardToAccountOwn;
 import utils.Base;
+import utils.FileHelper;
 import utils.Operations;
+import utils.SoftAssertCollector;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class PM015_CardToAccountOwn extends Base {
     @Then("I can select Own Card to Account and Navigate to the Transfer Details page")
@@ -14,5 +21,69 @@ public class PM015_CardToAccountOwn extends Base {
         Operations.click(PG014_CardToAccountOwn.navBarCardToAccount, driver);
         Operations.waitUntilElementIsVisible(PG014_CardToAccountOwn.selectOwnAccount, driver);
         Operations.click(PG014_CardToAccountOwn.selectOwnAccount, driver);
+    }
+    @Then("I can enter Amount {string}")
+    public void iCanEnterAmount(String amount) {
+        Operations.waitUntilElementIsVisible(PG014_CardToAccountOwn.amount, driver);
+        Operations.sendText(PG014_CardToAccountOwn.amount, amount, driver);
+    }
+    @Then("I click on the Next button without entering Remarks")
+    public void iClickOnTheNextButtonWithoutEnteringRemarks() {
+        Operations.waitUntilElementIsVisible(PG014_CardToAccountOwn.nextButton, driver);
+        Operations.click(PG014_CardToAccountOwn.availableBalance, driver);
+        Operations.waitUntilElementIsClickable(PG014_CardToAccountOwn.nextButton, driver);
+        Operations.scrollIntoElement(PG014_CardToAccountOwn.nextButton, driver);
+        Operations.click(PG014_CardToAccountOwn.nextButton, driver);
+    }
+    @Then("I can click on the Terms and Condition checkbox and Next button")
+    public void iCanClickOnTheTermsAndConditionCheckboxAndNextButton() {
+        Operations.waitUntilElementIsVisible(PG014_CardToAccountOwn.CheckBox, driver);
+        Operations.click(PG014_CardToAccountOwn.CheckBox, driver);
+        Operations.waitUntilElementIsClickable(PG014_CardToAccountOwn.SendOTP, driver);
+        Operations.click(PG014_CardToAccountOwn.SendOTP, driver);
+    }
+    @Then("I can Enter OTP {string} and click on the Next button")
+    public void iCanEnterOtpAndClickOnTheNextButton(String OTP) {
+        Operations.waitUntilElementIsVisible(PG014_CardToAccountOwn.EnterOTP, driver);
+        Operations.verifyElementIsPresent(PG014_CardToAccountOwn.EnterOTP, driver);
+        Operations.sendText(PG014_CardToAccountOwn.EnterOTP,OTP, driver);
+        Operations.waitUntilElementIsClickable(PG014_CardToAccountOwn.OTPNextButton, driver);
+        Operations.click(PG014_CardToAccountOwn.OTPNextButton, driver);
+    }
+
+    private Scenario scenario;
+
+    @io.cucumber.java.Before
+    public void before(Scenario scenario) {
+        this.scenario = scenario;
+    }
+
+    @Then("I can check transaction status {string} IF Transaction is failed then add an Screenshot")
+    public void iCanCheckTransactionStatus(String Status) throws Exception {
+        try {
+            Operations.waitUntilElementIsVisible(PG014_CardToAccountOwn.transactionStatus, driver);
+            Operations.verifyElementIsPresent(PG014_CardToAccountOwn.transactionStatus, driver);
+            Operations.matchText(PG014_CardToAccountOwn.transactionStatus, Status, driver);
+        } catch (AssertionError e) {
+            SoftAssertCollector.addError(e);
+            // Generate screenshot path
+            String screenshotPath = FileHelper.generateScreenshotFilePath("TransactionFailed");
+            // Take screenshot
+            Operations.takeSnapShot(driver, screenshotPath);
+
+            // Attach screenshot to report
+            try {
+                byte[] screenshotBytes = Files.readAllBytes(Paths.get(screenshotPath));
+                scenario.attach(screenshotBytes, "image/png", "TransactionFailed");
+            } catch (Exception ex) {
+                System.err.println("Failed to attach screenshot to report: " + ex.getMessage());
+            }
+        }
+    }
+
+    @Then("I can Back to the Home Page")
+    public void iCanBackToTheHomePage() {
+        Operations.waitUntilElementIsVisible(PG014_CardToAccountOwn.GoToHomeButton, driver);
+        Operations.click(PG014_CardToAccountOwn.GoToHomeButton, driver);
     }
 }
